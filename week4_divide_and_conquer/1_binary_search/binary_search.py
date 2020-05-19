2e88cbbcf81609c10958d9c8e2efcf2ceb73c57c
fernandoyto <fernando.toshioyto@gmail.com>
2020-05-18 23:59:05 -0300

    week 4 binary search done

--- week4_divide_and_conquer/1_binary_search/binary_search.py
+++ week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -2,14 +2,16 @@
 import sys
 
 def binary_search(a, x):
-    left, right = 0, len(a)
+    left, right = 0, len(a) - 1
     
-    while left < right:
-        mid = int((right + left) / 2)
+    while left <= right:
+        mid = (right + left) // 2
         
         if x == a[mid]:
             return mid
         elif x > a[mid]:
+            if x == a[left]:
+                return left
             left = mid + 1
         else:
             right = mid - 1
@@ -24,4 +26,4 @@ if __name__ == '__main__':
     m = data[n + 1]
     a = data[1 : n + 1]
     for x in data[n + 2:]:
-        print(binary_search(a, x), sep=' ')
+        print(binary_search(a, x), end = ' ')
