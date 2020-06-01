059a897ad7edadd1d9278f0f87c7f5adb42e49e5
fernandoyto <fernando.toshioyto@gmail.com>
2020-05-31 22:59:58 -0300

    week 5 last exercise i can do

--- week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,9 +1,22 @@
 # Uses python3
 import sys
+from math import inf
 
 def get_change(m):
-    #write your code here
-    return m // 4
+    possible_coins = [1, 3, 4]
+
+    minimum_coins = [None] * (m + 1)
+    minimum_coins[0] = 0
+
+    for m in range(1, m + 1):
+        minimum_coins[m] = inf
+        for coin in possible_coins:
+            if m >= coin:
+                total_coins = minimum_coins[m - coin] + 1
+                if total_coins < minimum_coins[m]:
+                    minimum_coins[m] = total_coins
+                
+    return minimum_coins[m]
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
